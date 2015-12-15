3acbb91a2131409452980fe5d69927134af40a90
Karin Fernandez <karinfdez@yahoo.com>
2015-12-14 23:54:42 -0500

    Fixed new user form

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,7 +17,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   
   
   version :thumb do
-    process :resize_to_fill => [50, 50]
+    process :resize_to_fill => [100, 100]
   end
   version :small do
     process :resize_to_fill => [361, 241]
