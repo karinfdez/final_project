a23561e1ecf3e8a758bf09373ad244d52fac03b7
Karin Fernandez <karinfdez@yahoo.com>
2016-02-26 15:24:50 -0500

    Added comments if valid fields in form

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,14 +2,14 @@ $(document).on("ready", function () {
 	
 	$(".js-submit").on('click',function (event) {  
 		event.preventDefault();
-		var post_id=parseInt($(".js-submit").val());
-		var comment=$("#comment_body").val();
-		var commenter=$("#comment_commenter").val();
-		console.log("HELOOOOOOO");
-		console.log(commenter);
+		   
+			var comment=$("#comment_body").val();
 
-		// alert("It works");
+			var commenter=$("#comment_commenter").val();
 
+		// if (comment && commenter){
+	if (comment && commenter){
+			var post_id=parseInt($(".js-submit").val());
 	    $.ajax({
 	        type: "POST",
 	        url: `/posts/${post_id}/comments`,
@@ -33,12 +33,16 @@ $(document).on("ready", function () {
 			success: function(resp){
 			    var last_element=resp.comments[resp.comments.length-1];
 			   	  $(".js-list-comments").append(`<li><strong>Comment added:</strong> <span data-livestamp="1456500513"></span><br>Name: ${last_element.commenter}<br>${last_element.body}<br></li>`)
-			  
+			  	  $(".error-message").empty();
 			},
 			error: function(){
 				alert("Error getting the messages.");
 			}
 	    });
+	  }
+	  else{
+	  	$(".error-message").text("Please fill the empty fields before submitting");
+	  }
 
 	});
 });
