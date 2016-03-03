fc31d746ec1a2078e5273b2a6379101b4d9be756
Karin Fernandez <karinfdez@yahoo.com>
2016-03-03 16:18:22 -0500

    Added time in javascript

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,5 +1,35 @@
 $(document).on("ready", function () {
 	
+	// Set time of messages:
+	function timeSince(date) {
+
+	    var seconds = Math.floor((new Date() - date) / 1000);
+
+	    var interval = Math.floor(seconds / 31536000);
+
+	    if (interval > 1) {
+	        return interval + " years";
+	    }
+	    interval = Math.floor(seconds / 2592000);
+	    if (interval > 1) {
+	        return interval + " months";
+	    }
+	    interval = Math.floor(seconds / 86400);
+	    if (interval > 1) {
+	        return interval + " days";
+	    }
+	    interval = Math.floor(seconds / 3600);
+	    if (interval > 1) {
+	        return interval + " hours";
+	    }
+	    interval = Math.floor(seconds / 60);
+	    if (interval > 1) {
+	        return interval + " minutes";
+	    }
+	    return Math.floor(seconds) + " seconds";
+    }
+
+
 	$(".js-submit").on('click',function (event) {  
 		event.preventDefault();
 		   
@@ -32,7 +62,7 @@ $(document).on("ready", function () {
 	        // data: {comment: { commenter: commenter, body: comment}},
 			success: function(resp){
 			    var last_element=resp.comments[resp.comments.length-1];
-			   	  $(".js-list-comments").append(`<li><strong>Comment added:</strong> <span data-livestamp="1456500513"></span><br>Name: ${last_element.commenter}<br>${last_element.body}<br></li>`)
+			   	  $(".js-list-comments").append(`<li><strong>Comment added:</strong> <span class='time-color'>${timeSince(Date.now())}</span><br>Name: ${last_element.commenter}<br>${last_element.body}<br></li>`)
 			  	  $(".error-message").empty();
 			},
 			error: function(){
