1f62947a153bc7e21af8a9db8aed9c96c8ee0310
Karin Fernandez <karinfdez@yahoo.com>
2016-03-03 14:52:37 -0500

    changed on carrierwave.rb and image_uploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,12 @@
 
 CarrierWave.configure do |config|
+
+	if Rails.env.production?
+   		config.storage = :fog
+ 	else
+  		 config.storage = :file
+ 	end	
+ 	
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
