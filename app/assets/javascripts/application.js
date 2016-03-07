2ff91b2df3ac2e7307bcb918240b1a3530b06878
Karin Fernandez <karinfdez@yahoo.com>
2016-03-07 15:36:38 -0500

    Removed extra info from google maps api script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require underscore
-//= require gmaps/google
+//= require gmaps/google  
 //= require chat
 //= require private_pub
 //= require_tree .
